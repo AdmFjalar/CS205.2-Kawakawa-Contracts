4228a18269128ee2e48dc5a3cf69b159b9ee82a3
Dylan Green <dylan.green801@gmail.com>
2024-05-17 11:46:29 +1200

    Refactored hash.js
    - Broke strings into constants
    - Replaced string literals with the newly declared constants in document.getElementById calls.

--- hash.js
+++ hash.js
@@ -1,23 +1,28 @@
-function CalculateHash(text) {
+const INPUT_TEXT1_ID = 'inputText1';
+const INPUT_TEXT2_ID = 'inputText2';
+const HASH_FORM_ID = 'hashForm';
+const HASH_RESULT_ID = 'hashResult';
+
+function calculateHash(text) {
     return sha3_512(text);
 }
 
-function CompareTexts(text1, text2) {
+function compareTexts(text1, text2) {
     if (calculateHash(text1) === calculateHash(text2)) {
         return "Texts match";
     }
     return "Texts do not match";
 }
 
-function FormHandler() {
-    document.getElementById('hashForm').addEventListener('submit', function(event) {
+function formHandler() {
+    document.getElementById(HASH_FORM_ID).addEventListener('submit', function(event) {
         event.preventDefault();
 
-        var inputText1 = document.getElementById('inputText1').value;
-        var inputText2 = document.getElementById('inputText2').value;
+        const inputText1 = document.getElementById(INPUT_TEXT1_ID).value;
+        const inputText2 = document.getElementById(INPUT_TEXT2_ID).value;
 
-        document.getElementById('hashResult').innerText = compareTexts(inputText1, inputText2);
+        document.getElementById(HASH_RESULT_ID).innerText = compareTexts(inputText1, inputText2);
     });
 }
 
-FormHandler();
+formHandler();
