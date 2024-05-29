69a0453610ae1dee0fbf50e899c86435fa2afaa2
Oliver Grönkrans <46197844+AdmFjalar@users.noreply.github.com>
2024-05-29 14:30:11 +1200

    Temporarily deactivated ignore in hash.js

--- hash.js
+++ hash.js
@@ -1,6 +1,3 @@
-const INPUT_TEXT1_ID = 'Document01';
-const INPUT_TEXT2_ID = 'Document02';
-const IGNORE_INDICES_ID = 'ignoreIndices';
 const HASH_FORM_ID = 'hashForm';
 const HASH_RESULT_ID = 'hashResult';
 
@@ -12,26 +9,26 @@ function ignoreText(unalteredText, ignoreIndices) {
     return unalteredText.split('').filter((char, index) => !ignoreIndices.includes(index)).join('');
 }
 
-function compareTexts(text1, text2, ignoreIndices) {
+function compareTexts(text1, text2/*, ignoreIndices*/) {
     let nonMatchingIndices = [];
     
-    for (let i = 0; i < Math.max(text1.length, text2.length); i++) {
+    /*for (let i = 0; i < Math.max(text1.length, text2.length); i++) {
        
         if (!ignoreIndices.includes(i) && text1[i] !== text2[i]) {
             nonMatchingIndices.push(i);
         }
-    }
+    }*/
 
-        const filteredText1 = ignoreText(text1, ignoreIndices);
-        const filteredText2 = ignoreText(text2, ignoreIndices);
+       // const filteredText1 = ignoreText(text1, ignoreIndices);
+       // const filteredText2 = ignoreText(text2, ignoreIndices);
     
        
-        const match = calculateHash(filteredText1) === calculateHash(filteredText2);
+        const match = calculateHash(text1/*filteredText1*/) === calculateHash(text2/*filteredText2*/);
         
-        return {
-            match,
-            nonMatchingIndices
-        };
+        return match;//{
+            //match,
+            //nonMatchingIndices
+        //};
 
 }
 
@@ -41,11 +38,11 @@ function formHandler() {
 
         const inputText1 = document.getElementById(INPUT_TEXT1_ID).value;
         const inputText2 = document.getElementById(INPUT_TEXT2_ID).value;
-        const ignoreIndicesInput = document.getElementById(IGNORE_INDICES_ID).value;
-        const ignoreIndices = ignoreIndicesInput.split(',').map(Number).filter(index => !isNaN(index));
+        //const ignoreIndicesInput = document.getElementById(IGNORE_INDICES_ID).value;
+        //const ignoreIndices = ignoreIndicesInput.split(',').map(Number).filter(index => !isNaN(index));
 
-        const result = compareTexts(inputText1, inputText2, ignoreIndices);
-        let resultText = result.match ? "Texts match" : `Texts do not match at indices: ${result.nonMatchingIndices.join(', ')}`;
+        const result = compareTexts(inputText1, inputText2/*, ignoreIndices*/);
+        let resultText = result.match ? "Texts match" : "Texts do not match";/*`Texts do not match at indices: ${result.nonMatchingIndices.join(', ')}`*/
         
         document.getElementById(HASH_RESULT_ID).innerText = resultText;
         
@@ -54,3 +51,4 @@ function formHandler() {
 }
 
 formHandler();
+
