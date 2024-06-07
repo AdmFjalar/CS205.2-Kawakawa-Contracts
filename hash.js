e4db08b208e6939b87e4676ae3eed4a0d6317c37
Tom Morrison <thomas.m.morrison.101@gmail.com>
2024-06-07 15:37:54 +1200

    added ignore box generator and random color generator

--- hash.js
+++ hash.js
@@ -4,9 +4,51 @@ const IGNORE_INDICES_ID = 'ignoreIndices';
 const HASH_FORM_ID = 'hashForm';
 const HASH_RESULT_ID = 'hashResult';
 
+var BACKGROUND_COLORS =[
+"linear-gradient(rgb(36, 113, 164),rgb(18, 56, 82))",
+"linear-gradient(rgb(171, 22, 92),rgb(85,11,46))",
+"linear-gradient(rgb(13, 206, 164),rgb(6,103,82))",
+"linear-gradient(rgb(146, 113, 75),rgb(73, 56, 57))",
+"linear-gradient(rgb(127, 29, 218),rgb(63,14,109))",
+"linear-gradient(rgb(169, 205, 27),rgb(84,103,13))"];
+
+let lastColors=[];
+
+function getRandomColors(){
+    var randomIndex;
+    do{
+        randomIndex =Math.floor(Math.random()*BACKGROUND_COLORS.length);
+    }
+    while (lastColors.includes(randomIndex));
+    lastColors.push(randomIndex);
+    if (lastColors.length>BACKGROUND_COLORS.length){
+        lastColors.shift();
+    }
+    return BACKGROUND_COLORS[randomIndex];
+}
+
+function generateIgnoreBox(){ 
+
+    //get orginal element
+    var original=document.getElementById('ignoreBox01');
+    //clone element and it's content
+    var clone=original.cloneNode(true);
+    //set new attributes like ID and color
+    clone.id ='ignoreBox02';
+    //get a new color
+    var newColor=getRandomColors();
+    clone.style.background=newColor;
 
 
-function toggleDisplay(testState){
+    //append
+    var targetDiv=document.getElementById('mismatchCenterBox01');
+    targetDiv.appendChild(clone);
+
+
+}
+
+
+function toggleDisplay(testState){ //toggles visibility of centre div section
     var blankCenterBox = document.querySelector('.blankCenterBox');
     var matchCenterBox = document.querySelector('.matchCenterBox');
     var mismatchCenterBox = document.querySelector('.mismatchCenterBox');
@@ -78,10 +120,12 @@ function formHandler() {
         
         else{
             toggleDisplay("mismatch");
+            generateIgnoreBox();
+            
         }
         
 
-        document.getElementById(HASH_RESULT_ID).innerText = resultText;
+        //document.getElementById(HASH_RESULT_ID).innerText = resultText;
         
     });
 
