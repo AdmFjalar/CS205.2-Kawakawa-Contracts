af4bdca1fbda1bd42ac5adf6f8e678674211e1e9
Oliver Grönkrans <oliver@gronkrans.com>
2024-06-14 12:23:52 +1200

    Removed spaces and unused code

--- hash.js
+++ hash.js
@@ -5,87 +5,86 @@ const HASH_FORM_ID = 'hashForm';
 const HASH_RESULT_ID = 'hashResult';
 
 var BACKGROUND_COLORS =[
-"rgb(36,113,164)",
-"rgb(13,206,164)",
-"rgb(127,29,218)"];
-
-let lastColors=[];
-
-function getRandomColors(){ //fetches an RGB code from the array; dosen't re-use any colors till entire array is used
-    var randomIndex;
-    do{
-        randomIndex =Math.floor(Math.random()*BACKGROUND_COLORS.length);
-    }
-    while (lastColors.includes(randomIndex));
-    lastColors.push(randomIndex);
-    if (lastColors.length>BACKGROUND_COLORS.length){
-        lastColors.shift();
-    }
-    return BACKGROUND_COLORS[randomIndex];
-}
-
-function darkenColor(rgb,percent){ //darkens RGB by specified percentage and returns new rgb code
-    const[r,g,b]=rgb.match(/\d+/g).map(Number);
-
-    const factor=1-percent/100;
-    const newR=Math.max(0,Math.min(255,Math.floor(r*factor)));
-    const newG=Math.max(0,Math.min(255,Math.floor(g*factor)));
-    const newB=Math.max(0,Math.min(255,Math.floor(b*factor)));
-    return "rgb(${newR},${newG},${newB})";
-}
-
-function createGradient(rgb){ //takes an RGB, generates a darker version and creates a gradient 
-    const darkerColor=darkenColor(rgb, 50);
-    return "linear-gradient(${rbg}, ${darkerColor})";
-}
-
-function generateIgnoreBox(){ 
-
-    //get orginal element
-    var original=document.getElementById('ignoreBox01');
-    //clone element and it's content
-    var clone=original.cloneNode(true);
-    //set new attributes like ID and color
-    clone.id ='ignoreBox02';
-    //get a new color
-    var newColor=getRandomColors(); //returns a single colour from the list
-    var newGradient=createGradient(newColor);//turns this into a gradient
-    
-
-    clone.style.background=newGradient;
-
-
-    //append
-    var targetDiv=document.getElementById('mismatchCenterBox01');
-    targetDiv.appendChild(clone);
-
-
-}
-
-
-function toggleDisplay(testState){ //toggles visibility of centre div section
-    var blankCenterBox = document.querySelector('.blankCenterBox');
-    var matchCenterBox = document.querySelector('.matchCenterBox');
-    var mismatchCenterBox = document.querySelector('.mismatchCenterBox');
-    
-    if (testState === "mismatch"){
-        blankCenterBox.style.display='none';
-        matchCenterBox.style.display='none';
-        mismatchCenterBox.style.display='block'; 
-    }
-    else if(testState=== "match" ){
-        blankCenterBox.style.display='none';
-        matchCenterBox.style.display='block';
-        mismatchCenterBox.style.display='none'; 
-    }
-    else
-    {
-        blankCenterBox.style.display='block';
-        matchCenterBox.style.display='none';
-        mismatchCenterBox.style.display='none'; 
-    }
-    }
-
+  "rgb(36,113,164)",
+  "rgb(13,206,164)",
+  "rgb(127,29,218)"];
+  
+  let lastColors=[];
+  
+  function getRandomColors(){ //fetches an RGB code from the array; dosen't re-use any colors till entire array is used
+      var randomIndex;
+      do{
+          randomIndex =Math.floor(Math.random()*BACKGROUND_COLORS.length);
+      }
+      while (lastColors.includes(randomIndex));
+      lastColors.push(randomIndex);
+      if (lastColors.length>BACKGROUND_COLORS.length){
+          lastColors.shift();
+      }
+      return BACKGROUND_COLORS[randomIndex];
+  }
+  
+  function darkenColor(rgb,percent){ //darkens RGB by specified percentage and returns new rgb code
+      const[r,g,b]=rgb.match(/\d+/g).map(Number);
+  
+      const factor=1-percent/100;
+      const newR=Math.max(0,Math.min(255,Math.floor(r*factor)));
+      const newG=Math.max(0,Math.min(255,Math.floor(g*factor)));
+      const newB=Math.max(0,Math.min(255,Math.floor(b*factor)));
+      return "rgb(${newR},${newG},${newB})";
+  }
+  
+  function createGradient(rgb){ //takes an RGB, generates a darker version and creates a gradient 
+      const darkerColor=darkenColor(rgb, 50);
+      return "linear-gradient(${rbg}, ${darkerColor})";
+  }
+  
+  function generateIgnoreBox(){ 
+  
+      //get orginal element
+      var original=document.getElementById('ignoreBox01');
+      //clone element and it's content
+      var clone=original.cloneNode(true);
+      //set new attributes like ID and color
+      clone.id ='ignoreBox02';
+      //get a new color
+      var newColor=getRandomColors(); //returns a single colour from the list
+      var newGradient=createGradient(newColor);//turns this into a gradient
+      
+  
+      clone.style.background=newGradient;
+  
+  
+      //append
+      var targetDiv=document.getElementById('mismatchCenterBox01');
+      targetDiv.appendChild(clone);
+  
+  
+  }
+  
+  function toggleDisplay(testState){ //toggles visibility of centre div section
+      var blankCenterBox = document.querySelector('.blankCenterBox');
+      var matchCenterBox = document.querySelector('.matchCenterBox');
+      var mismatchCenterBox = document.querySelector('.mismatchCenterBox');
+      
+      if (testState === "mismatch"){
+          blankCenterBox.style.display='none';
+          matchCenterBox.style.display='none';
+          mismatchCenterBox.style.display='block'; 
+      }
+      else if(testState=== "match" ){
+          blankCenterBox.style.display='none';
+          matchCenterBox.style.display='block';
+          mismatchCenterBox.style.display='none'; 
+      }
+      else
+      {
+          blankCenterBox.style.display='block';
+          matchCenterBox.style.display='none';
+          mismatchCenterBox.style.display='none'; 
+      }
+      }
+  
 function calculateHash(text) {
     return sha3_512(text);
 }
@@ -175,18 +174,18 @@ function diff(text1, text2) {
     }).join('');
 }
 
+function swapInputsAndParagraphs ()
+{
+  
+}
+
 function formHandler() {
     document.getElementById(HASH_FORM_ID).addEventListener('submit', function(event) {
         event.preventDefault();
 
         const inputText1 = document.getElementById(INPUT_TEXT1_ID).value;
-        const inputText2 = document.getElementById(INPUT_TEXT2_ID).value;
-
-        //const ignoreIndicesInput = document.getElementById(IGNORE_INDICES_ID).value;
-        //const ignoreIndices = ignoreIndicesInput.split(',').map(Number).filter(index => !isNaN(index));
-
+        const inputText2 = document.getElementById(INPUT_TEXT2_ID).value; 
         const result = compareTexts(inputText1, inputText2/*, ignoreIndices*/);
-        let resultText = result.match ? "Texts match" : "Texts do not match";/*`Texts do not match at indices: ${result.nonMatchingIndices.join(', ')}`*/
         
         if (result){
             toggleDisplay("match");
@@ -198,12 +197,11 @@ function formHandler() {
             
         }
         
-
-        
         //const result = compareTexts(inputText1, inputText2);
         //var diffOutput = diff(inputText1, inputText2);
         //wrapIndexesWithColors(INPUT_TEXT1_ID, INPUT_TEXT2_ID, diffOutput);
     });
 
 }
+
 formHandler();
